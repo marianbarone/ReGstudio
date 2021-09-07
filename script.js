828bd06063c7aa409a6c87996a36e89d1798e108
marian.barone <marian.barone@gmail.com>
2021-09-07 19:19:17 -0300

    Index terminada con modal y carousel

---
+++ script.js
@@ -0,0 +1,42 @@
+var $modalCCLaRioja = $('#modalCCLaRioja').modal({
+    show: false
+});
+$('.overlayCCLaRioja').on('click', function() {
+    $modalCCLaRioja.modal('show');
+});
+
+var $modalArtMaria = $('#modalArtMaria').modal({
+    show: false
+});
+$('.overlayArtMaria').on('click', function() {
+    $modalArtMaria.modal('show');
+});
+
+var $modalCCEnriqueta = $('#modalCCEnriqueta').modal({
+    show: false
+});
+$('.overlayCCEnriqueta').on('click', function() {
+    $modalCCEnriqueta.modal('show');
+});
+
+var $modalEdificioManzanares = $('#modalEdificioManzanares').modal({
+    show: false
+});
+$('.overlayEdificioManzanares').on('click', function() {
+    $modalEdificioManzanares.modal('show');
+});
+
+var $modalKeyBiscayne = $('#modalKeyBiscayne').modal({
+    show: false
+});
+$('.overlayKeyBiscayne').on('click', function() {
+    $modalKeyBiscayne.modal('show');
+});
+
+var $modalPCCHouses = $('#modalPCCHouses').modal({
+    show: false
+});
+$('.overlayPCCHouses').on('click', function() {
+    $modalPCCHouses.modal('show');
+});
+
