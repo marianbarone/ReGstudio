1b419b0e994cddeddc7e389c6ddb8ad851a55755
mbarone1 <mbarone@activar.app>
2021-09-19 12:53:48 -0300

    Proyectos terminado

--- script.js
+++ script.js
@@ -40,3 +40,65 @@ $('.overlayPCCHouses').on('click', function() {
     $modalPCCHouses.modal('show');
 });
 
+var $modalForestCondo = $('#modalForestCondo').modal({
+    show: false
+});
+$('.overlayForestCondo').on('click', function() {
+    $modalForestCondo.modal('show');
+});
+
+var $modalOFViLo = $('#modalOFViLo').modal({
+    show: false
+});
+$('.overlayOFViLo').on('click', function() {
+    $modalOFViLo.modal('show');
+});
+
+var $modalCasaSaavedra = $('#modalCasaSaavedra').modal({
+    show: false
+});
+$('.overlayCasaSaavedra').on('click', function() {
+    $modalCasaSaavedra.modal('show');
+});
+
+var $modalCasaTR = $('#modalCasaTR').modal({
+    show: false
+});
+$('.overlayCasaTR').on('click', function() {
+    $modalCasaTR.modal('show');
+});
+
+var $modalCasaMT = $('#modalCasaMT').modal({
+    show: false
+});
+$('.overlayCasaMT').on('click', function() {
+    $modalCasaMT.modal('show');
+});
+
+var $modalCasaA08 = $('#modalCasaA08').modal({
+    show: false
+});
+$('.overlayCasaA08').on('click', function() {
+    $modalCasaA08.modal('show');
+});
+
+var $modalCasaMG = $('#modalCasaMG').modal({
+    show: false
+});
+$('.overlayCasaMG').on('click', function() {
+    $modalCasaMG.modal('show');
+});
+
+var $modalCasa86 = $('#modalCasa86').modal({
+    show: false
+});
+$('.overlayCasa86').on('click', function() {
+    $modalCasa86.modal('show');
+});
+
+var $modalCasaF = $('#modalCasaF').modal({
+    show: false
+});
+$('.overlayCasaF').on('click', function() {
+    $modalCasaF.modal('show');
+});
